45afde6d396c3f1ad70edbca1c351704f915cf1e
KozhanMykhailo <mihasikk98@gmail.com>
2020-12-23 16:20:57 +0200

    add Authenticate

---
+++ Car.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Car
+{
+	public string Color { get; set; }
+	public string Model { get; set; }
+	public decimal RentalPrice { get; set; }
+	public ClassCar Class { get; set; }
+
+}
---
+++ Enums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public enum Color 
+{
+	Yellow = "Желтый",
+	Red = "Крастный",
+
+
+}
+
+public enum ClassCar
+{
+	/// <summary>
+	///  Малогабаритные авто
+	/// </summary>
+	A = 0,
+	/// <summary>
+	/// Малолитражки
+	/// </summary>
+	B = 1,
+	/// <summary>
+	/// Средние по размеру 
+	/// </summary>
+	C = 2,
+	/// <summary>
+	/// Комфорт
+	/// </summary>
+	D = 3,
+	/// <summary>
+	/// Премиум
+	/// </summary>
+	E = 4,
+}
