1d4d09e2a243992bfc08186c793e38bdba40a3a2
KozhanMykhailo <mihasikk98@gmail.com>
2020-12-23 16:20:57 +0200

    add Authenticate

---
+++ CarRental/Entities/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities
+{	public enum ClassCar
+	{
+		/// <summary>
+		///  Малогабаритные авто
+		/// </summary>
+		A = 0,
+		/// <summary>
+		/// Малолитражки
+		/// </summary>
+		B = 1,
+		/// <summary>
+		/// Средние по размеру 
+		/// </summary>
+		C = 2,
+		/// <summary>
+		/// Комфорт
+		/// </summary>
+		D = 3,
+		/// <summary>
+		/// Премиум
+		/// </summary>
+		E = 4,
+	}
+
+}
